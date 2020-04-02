20b79fa0bb189889212cd399acc3a85fe9d4debb
And9626 <62510802+And9626@users.noreply.github.com>
2020-04-02 19:47:03 +0200

    Add files via upload

--- src/ex_4_client.cpp
+++ src/ex_4_client.cpp
@@ -7,14 +7,14 @@
 int main (int argc, char **argv) {
 
 	ros::init(argc, argv, "controller_action_client");
-  if(argc != 3){
+  if(argc != 5){
 		ROS_INFO("%d",argc);
-		ROS_WARN("Usage: demo_action_client <goal> <time_to_preempt_in_sec>");
+		ROS_WARN("Usage: demo_action_client <init_pos> <goal> <max_velocity> <time_to_preempt_in_sec>");
 		return 1;
 	}
 
 
-  actionlib::SimpleActionClient<exercise_4::controllerAction> ac("controller_action", true);
+  actionlib::SimpleActionClient<exercise_4::controllerAction> ac("c_action", true);
 
   ROS_INFO("Waiting for action server to start.");
 
@@ -25,16 +25,19 @@ int main (int argc, char **argv) {
 
 
   exercise_4::controllerGoal goal;
-  
-  goal.position = atoi(argv[1]);
+   
+  goal.init_pos = atof(argv[1]);
+  goal.position = atof(argv[2]);
+  goal.max_velocity = atof(argv[3]);
+ 
 
 	
  
-  ROS_INFO("Sending Goal [%d] and Preempt time of [%d]",goal.position, atoi(argv[2]));
+  ROS_INFO("Sending Goal [%f] and Preempt time of [%f]",goal.position, atof(argv[4]));
   ac.sendGoal(goal);
 
 
-  bool finished_before_timeout = ac.waitForResult(ros::Duration(atoi(argv[2])));
+  bool finished_before_timeout = ac.waitForResult(ros::Duration(atof(argv[4])));
   ac.cancelGoal();
 
   if (finished_before_timeout) {
--- src/ex_4_server.cpp
+++ src/ex_4_server.cpp
@@ -10,15 +10,15 @@ class action_class {
 	private:
   	ros::NodeHandle nh_;
   	actionlib::SimpleActionServer<exercise_4::controllerAction> as; 
-  	ros_action::controllerFeedback feedback;
-	ros_action::controllerResult result;
+  	exercise_4::controllerFeedback feedback;
+	exercise_4::controllerResult result;
 
   	std::string action_controller;
-  	int goal;
-  	int progress = atoi(argv[1]);
+  	float goal;
+  	
 
 	public:
-		action_class(std::string name) :
+		action_class(std::string controller) :
   	  as(nh_, controller, boost::bind(&action_class::executeCB, this, _1), false),
   	  action_controller(controller) {
 				as.registerPreemptCallback(boost::bind(&action_class::preemptCB, this));
@@ -26,15 +26,18 @@ class action_class {
   		}
 	
 		void preemptCB(){
+			exercise_4::controllerGoal current;			
 			ROS_WARN("%s got preempted!", action_controller.c_str());
-			result.final_position = progress;
+			result.final_position = current.position;
 			as.setPreempted(result,"I got Preempted"); 
   	}
   
-		void executeCB(const ros_action::controllerGoalConstPtr &goal) {
+		void executeCB(const exercise_4::controllerGoalConstPtr &goal) {
 			if(!as.isActive() || as.isPreemptRequested()) return;
 			ros::Rate rate(10);
 			ROS_INFO("%s is processing the goal %f", action_controller.c_str(), goal->position);
+
+			float progress=goal->init_pos;	
 			float control_action;
 				do{
 				if (!ros::ok()) {
@@ -49,29 +52,40 @@ class action_class {
 				}	
 
 				if(goal->position == progress) {
-					ROS_INFO("%s Succeeded at getting to goal %d", action_controller.c_str(), goal->position);
+					ROS_INFO("%s Succeeded at getting to goal %f", action_controller.c_str(), goal->position);
 					result.final_position = progress;
 					as.setSucceeded(result);
 				}
 				else {
-					ROS_INFO("Setting to goal %d / %d",feedback.current_position,goal->position);
+					ROS_INFO("Setting to goal %f / %f",feedback.current_position,goal->position);
 					feedback.current_position = progress;
 					as.publishFeedback(feedback);
 			}
-		control_action = atoi(argv[2])/10;
-		if(progress>goal->position)
+		
+		control_action = (goal->max_velocity)/10;
+
+		if(progress>goal->position && (progress-goal->position)>control_action)
 			progress = progress - control_action;
-		if(progress<goal->position)
+		if(progress>goal->position && (progress-goal->position)<control_action)
+			progress = progress - (progress-goal->position);
+		if(progress<goal->position && (goal->position-progress)>control_action)
 			progress = progress + control_action;
+		if(progress<goal->position && (goal->position-progress)<control_action)
+			progress = progress + (goal->position-progress);
 		rate.sleep();
-		}while( progress != goal->position)	
+		}while( fabs(goal->position-progress) > 0.01);	
+	
+	result.final_position = progress;
+	as.setSucceeded(result);
+	ROS_INFO("%s Succeeded at getting to goal %f", action_controller.c_str(), result.final_position);
+
   }
 };
 
 int main(int argc, char** argv)
 {
 
-  ros::init(argc, argv, "controller_action");
+  ros::init(argc, argv, "c_action");
   ROS_INFO("Starting Controller");
   action_class controller_action_obj(ros::this_node::getName());
   ros::spin();
